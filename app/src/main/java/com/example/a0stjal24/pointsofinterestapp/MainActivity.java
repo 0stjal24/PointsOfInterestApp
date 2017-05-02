541b9d5ea394a66fe0882ba80bf9e74e74ed6e1c
0stjal24 <0stjal24@solent.ac.uk>
2017-05-02 14:21:32 +0100

    Question 2 p[rogress

--- app/src/main/java/com/example/a0stjal24/pointsofinterestapp/MainActivity.java
+++ app/src/main/java/com/example/a0stjal24/pointsofinterestapp/MainActivity.java
@@ -11,20 +11,16 @@ import android.view.MenuItem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.OverlayItem;
-
-
-
-
-
-
-
-
 
 
 import java.util.ArrayList;
@@ -36,31 +32,29 @@ public class MainActivity extends AppCompatActivity
     ItemizedIconOverlay.OnItemGestureListener<OverlayItem> markerGestureListener;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addpoi);
+
 
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
         setContentView(R.layout.activity_main);
 
-        mv = (MapView)findViewById(R.id.map1);
+        mv = (MapView) findViewById(R.id.map1);
 
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(11);
         mv.getController().setCenter(new GeoPoint(50.9068, -1.3943));
 
-        markerGestureListener = new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>()
-        {
-            public boolean onItemLongPress(int i, OverlayItem item)
-            {
+        markerGestureListener = new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+            public boolean onItemLongPress(int i, OverlayItem item) {
                 Toast.makeText(MainActivity.this, item.getSnippet(), Toast.LENGTH_SHORT).show();
                 return true;
             }
 
-            public boolean onItemSingleTapUp(int i, OverlayItem item)
-            {
-                Toast.makeText(MainActivity.this, item.getSnippet(), Toast.LENGTH_SHORT). show();
+            public boolean onItemSingleTapUp(int i, OverlayItem item) {
+                Toast.makeText(MainActivity.this, item.getSnippet(), Toast.LENGTH_SHORT).show();
                 return true;
             }
         };
@@ -68,4 +62,42 @@ public class MainActivity extends AppCompatActivity
         items = new ItemizedIconOverlay<OverlayItem>(this, new ArrayList<OverlayItem>(), null);
         mv.getOverlays().add(items);
     }
+
+        public boolean onCreateOptionsMenu(Menu menu)
+        {
+            MenuInflater inflater=getMenuInflater();
+            inflater.inflate(R.menu.menu, menu);
+            return true;
+        }
+        public boolean onOptionsItemSelected(MenuItem item)
+        {
+            if(item.getItemId() == R.id.addpoi)
+            {
+                Intent intent = new Intent(this,PoiAdd.class);
+                startActivityForResult(intent,0);
+                return true;
+            }
+            return false;
+        }
+
+        protected void onActivityResult(int requestCode,int resultCode,Intent intent)
+        {
+            if(requestCode==0)
+            {
+                if (requestCode==RESULT_OK)
+                {
+                    Bundle extras=intent.getExtras();
+                    boolean addpoi = extras.getBoolean("com.example.a0stjal24.pointofinterestapp");
+                    if(addpoi==true)
+                    {
+                        mv.setTileSource(TileSourceFactory.ADDPOI);
+                    }
+                    else
+                    {
+                        mv.setTileSource(TileSourceFactory.MAPNIK);
+                    }
+                }
+        }
+
+
 }
